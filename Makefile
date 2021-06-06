0462cb582f1569982ca4394ea38c26c60793310f
andrew <afoote97@gmail.com>
2021-06-06 14:08:46 -0700

    add testing framework
    tests are based on assembly snippets with testing annotations.
    
    the assembly snippets are assembled into executable binaries. run.sh
    executes the binary (currently just in the simulator), then compares
    the final processor state to the expected state declared in the
    annotation in the assembly file.
    
    the makefile generates a list of all tests by finding all .s files in
    tests/, and can assemble and run the tests.

--- Makefile
+++ Makefile
@@ -1,6 +1,34 @@
 CFLAGS = -g -Wall
 
-sim: sim.o
+TEST_SOURCES := $(shell find tests -name '*.s')
+TEST_BINARIES := $(patsubst tests/%.s,build/%.bin,$(TEST_SOURCES))
+TEST_LOGS := $(patsubst %.bin,%.log,$(TEST_BINARIES))
+
+sim: build/sim.o
+	$(CC) -o $@ $<
+
+build/%.o: %.c
+	mkdir -p build
+	$(CC) -c -o $@ $<
+
+$(TEST_BINARIES): build/%.bin: tests/%.s
+	mkdir -p $(@D)
+	bash -c "./as.py <(grep -v -e '^!' $<) $@ > $@.build.log"
+
+.PHONY: $(TEST_LOGS)
+$(TEST_LOGS): build/%.log: build/%.bin tests/%.s sim
+	(bash ./tests/run.sh $(word 2,$^) $(word 1,$^) || echo "test $(word 2,$^) failed: $@") | tee $@
+
+run-%: build/%.bin
+	echo "running test for $*"
+
+build-tests: $(TEST_BINARIES)
+
+test: $(TEST_LOGS)
+	@echo sources: $(TEST_SOURCES)
+	@echo binaries: $(TEST_BINARIES)
+	@echo run: $(TEST_LOGS)
 
 clean:
-	rm sim sim.o
+	rm -f sim sim.o
+	rm -rf build
---
+++ tests/run.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+CLEAR='\033[0m'
+
+source="$1"
+binary="$2"
+
+sim="./sim"
+sim_args=""
+sim_log="${binary%.*}.sim.log"
+end_log="${binary%.*}.end.sim.log"
+
+expected=()
+
+while read -r cmd args; do
+    if [ $cmd = '!rand' ]; then
+        sim_args="$sim_args -r";
+    fi
+
+    if [ $cmd = '!expect' ]; then
+        expected+=("$args");
+    fi
+
+done < <(grep '!' $source)
+
+echo "sim_args $sim_args"
+echo "expected"
+declare -p expected
+
+output=`$sim $sim_args $binary`
+
+
+echo "simulator log: $sim_log"
+echo "$output">$sim_log
+
+echo "end log: $end_log"
+end=`echo "$output" | grep -A500 'Finished.'`
+echo "$end">$end_log
+
+for line in "${expected[@]}"; do
+    echo -n "expecting to see '$line'...";
+
+    if echo "$end" | grep -q "$line"; then
+        echo " found."
+    else
+        echo " not found."
+        echo -e "${RED}FAILED${CLEAR} $source"
+        exit 1
+    fi
+done
+
+echo -e "${GREEN}PASSED${CLEAR} $source"
