a38b89e6382a388002987f78e2b3f4fe859e76c8
andrew <afoote97@gmail.com>
2022-02-24 22:44:42 -0800

    use origin = in fin-0.s
    replace nop padding with origin directive

--- tests/fin/fin-0.s
+++ tests/fin/fin-0.s
@@ -7,29 +7,10 @@ LDM 7
 XCH 1
 / 0x04
 FIN 3P
-NOP
-NOP
-NOP
-/ 0x08
-NOP
-NOP
-NOP
-NOP
-/ 0x0c
-NOP
-NOP
-NOP
-NOP
-/ 0x10
-NOP
-NOP
-NOP
-NOP
-/ 0x14
-NOP
-NOP
-NOP
-42
+
+/ 0x17
+= 23
+42 / 0x2a
 / 0x18
 !expect register  6: 0x2
 !expect register  7: 0xa
