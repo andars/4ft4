cb4eba55795fd8e28809304a00a558d6456ed7da
andrew <afoote97@gmail.com>
2022-03-06 19:11:53 -0800

    merge some cases in ram inst decoder

--- rtl/ram.v
+++ rtl/ram.v
@@ -78,13 +78,7 @@ always @(*) begin
             4'h0: begin
                 write_ram = 1;
             end
-            4'h8: begin
-                ram_to_data = 1;
-            end
-            4'h9: begin
-                ram_to_data = 1;
-            end
-            4'hb: begin
+            4'h8, 4'h9, 4'hb: begin
                 ram_to_data = 1;
             end
             default: begin
