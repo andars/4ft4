a8e7e131f526b51e222cfab3f4bb0ccebdb9057d
andrew <afoote97@gmail.com>
2022-03-17 02:43:26 -0700

    integrate spoke modules into icebreaker_top
    this provides a uart interface to the backdoors wishbone interfaces of
    the rom and ram.
    
    it is working on icebreaker to reload the program in rom through the
    backdoor (without reflashing the fpga) and read from ram.
    
    reduce rom size to 128 bytes in synthesis for now.

--- rtl/icebreaker_top.v
+++ rtl/icebreaker_top.v
@@ -3,7 +3,9 @@
 module icebreaker_top(
     input clock,
     input test_,
-    output [3:0] leds
+    output [3:0] leds,
+    input serial_rx,
+    output serial_tx
 );
 
 wire reset;
@@ -11,19 +13,40 @@ wire [3:0] rom_out;
 
 assign leds = rom_out;
 
+wire [31:0] data_to_sys;
+wire [31:0] data_from_sys;
+wire ack;
+wire [31:0] addr;
+wire cyc;
+wire strobe;
+wire we;
+
 wb_system sys(
     .clock(clock),
     .reset(reset),
     .rom_out(rom_out),
 
-    // TODO: connect these to spoke
-    .wb_data_i(32'h0),
-    .wb_addr_i(32'h0),
-    .wb_cyc_i(1'b0),
-    .wb_strobe_i(1'b0),
-    .wb_we_i(1'b0),
-    .wb_data_o(),
-    .wb_ack_o()
+    .wb_data_i(data_to_sys),
+    .wb_addr_i(addr),
+    .wb_cyc_i(cyc),
+    .wb_strobe_i(strobe),
+    .wb_we_i(we),
+    .wb_data_o(data_from_sys),
+    .wb_ack_o(ack)
+);
+
+uart_wb_master wb_master(
+    .clock(clock),
+    .reset(reset),
+    .serial_rx(serial_rx),
+    .serial_tx(serial_tx),
+    .data_in(data_from_sys),
+    .data_out(data_to_sys),
+    .ack_in(ack),
+    .addr_out(addr),
+    .cyc_out(cyc),
+    .strobe_out(strobe),
+    .we_out(we)
 );
 
 // Reset generator
